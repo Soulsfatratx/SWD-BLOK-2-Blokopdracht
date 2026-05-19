bf4bf8b247b0aeef8ff18c4af48bcf4b86fb888d
Manuel van Leeuwen <m.vanleeuwen12@student.han.nl>
2026-05-19 13:34:42 +0200

    Add tetromino module

--- tetromino.py
+++ tetromino.py
@@ -1,26 +1,56 @@
-# tetromino.py
-# Verantwoordelijke: Manuel
+'''
+Tetromino module - Manuel
+'''
 
-# VARIABELEN
-# SHAPES = { ... }
-# dict met 7 blokvormen als 2D-lijsten
+import random
 
-# COLORS = { ... }
-# kleur per bloknaam
+# 2D arrays of all possible shapes
+SHAPES = {
+    'I': [[1, 1, 1, 1]],
+    'O': [[1, 1], [1, 1]],
+    'T': [[0, 1, 0], [1, 1, 1]],
+    'S': [[0, 1, 1], [1, 1, 0]],
+    'Z': [[1, 1, 0], [0, 1, 1]],
+    'J': [[1, 0, 0], [1, 1, 1]],
+    'L': [[0, 0, 1], [1, 1, 1]]
+}
+
+# Colors matching with their shapes
+COLORS = {
+    'I': 'cyan',
+    'O': 'yellow',
+    'T': 'purple',
+    'S': 'green',
+    'Z': 'red',
+    'J': 'blue',
+    'L': 'orange'
+}
 
 
-# FUNCTIES
+def get_shape(name):
+    '''Get a 2D array representing a shape'''
 
-def get_shape(naam):
-    # geeft 2D-lijst terug van een blok
-    pass
+    return SHAPES[name]
 
 
 def rotate(shape):
-    # draait een 2D-lijst 90 graden
-    pass
+    '''Rotate a shape 90 degrees to the right'''
+
+    height = len(shape)
+    width = len(shape[0])
+
+    match (height, width):
+        case (1, 4) | (4, 1):
+            return [[shape[y][x] for y in range(len(shape))] for x in range(len(shape[0]))]
+        case (2, 2):
+            return [[shape[x][y] for y in range(len(shape))] for x in range(len(shape[0]))]
+        case (2, 3):
+            return [[shape[1 - y][x] for y in range(len(shape))] for x in range(len(shape[0]))]
+        case (3, 2):
+            return [[shape[2 - y][x] for y in range(len(shape))] for x in range(len(shape[0]))]
 
 
 def get_random_shape():
-    # kiest willekeurig een blokvorm
-    pass
+    '''Get a random shape'''
+
+    return random.choice(list(SHAPES.values()))
